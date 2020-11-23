d77947fbf6d906c303158895b952808d33b4b929
Jatmika Teja <jatmikatejasukmana@gmail.com>
2020-11-23 22:57:14 +0700

    Add api_text and api_user

---
+++ api_text/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.get('/test', (req, res) => {
+  res.status(200).json({
+    name: 'api_text',
+    time: new Date(),
+  });
+});
+
+app.listen(6969, () => {
+  console.log('on port 6969');
+});
