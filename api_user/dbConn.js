df1074941a41bdb5d6aae87c511f05c32f9edbbe
Jatmika Teja <jatmikatejasukmana@gmail.com>
2020-12-01 14:36:05 +0700

    module.exports pls.

--- api_user/dbConn.js
+++ api_user/dbConn.js
@@ -1,9 +1,13 @@
 const {Pool} = require('pg');
 
-export const pool = new Pool({
+const pool = new Pool({
   user: process.env.POSTGRES_PASSWORD,
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DATABASE,
   password: process.env.POSTGRES_PASSWORD,
   port: 5432,
 });
+
+module.exports = {
+  pool,
+};
