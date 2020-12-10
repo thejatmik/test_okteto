95ab06b86d3bcdfd12a00eb5415d42746f04896a
Jatmika Teja <jatmikatejasukmana@gmail.com>
2020-12-10 16:14:43 +0700

    add status component

--- fe_yes/src/components/Status.vue
+++ fe_yes/src/components/Status.vue
@@ -2,16 +2,30 @@
 <div>
   <p>Le status</p>
   <p>{{ userAPI }}</p>
+  <p>{{ status }}</p>
 </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'Status',
   data() {
     return {
       userAPI: process.env.VUE_APP_USER_API,
+      status: {},
     };
+  },
+  created() {
+    (async () => {
+      try {
+        const { data } = await axios.get(this.userAPI);
+        this.status = data;
+      } catch (err_) {
+        this.status = 'Error';
+      }
+    })();
   },
 };
 </script>
